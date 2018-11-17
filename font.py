8cc187b5ea5c2a88ec3c3250d6df8c543cd83b74
Kerma <kerma@ionizing-radar.ca>
2018-11-17 22:40:17 +0000

    font.py loads from file and saves serialized version to file

--- font.py
+++ font.py
@@ -16,40 +16,42 @@
 #   [(False, True, False, True, True), (False, True, True, False, True), (False, False, True, True, True)]]
 
 import sys, argparse
+import pickle
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--infile", help="infile")
-parser.add_argument("-o", "--outfile", help="outfile")
-args = parser.parse_args()
+def loadfont(infile):
+    with open(infile, 'r') as f:
+        character_dict = dict()
+        for line in f:
+            if len(line) < 2: break
+            row = []
+            key = None
+            tuple = line.rstrip().split(',')
+            for element in tuple:
+                if (tuple.index(element) == 0 and key == None):
+                    key = chr(int(element))
+                else:
+                    this_row = []
+                    for col_counter in range(len(element)):
+                            this_bit = True if element[col_counter] == '1' else False
+                            this_row.append(this_bit)
+                    row.append(this_row)
+            character_dict[key] = [row,list(zip(*row))]
+    return character_dict
 
-if not (args.infile and args.outfile):
-    print("missing parameters, use -h for help")
-    exit()
+def savefont(character_dict, outfile):
+    pickle.dump(character_dict, open(outfile, 'wb'))
+    return
 
-character_dict = dict()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--infile", help="infile")
+    parser.add_argument("-o", "--outfile", help="outfile")
+    args = parser.parse_args()
 
-with open(args.infile, 'r') as f:
-    for line in f:
-        if len(line) < 2: break
-        row = []
-        key = None
-        tuple = line.rstrip().split(',')
-        rows = len(tuple)-1     # have to subtract 1 because the element in the tuple is the dictionary key
-        cols = len(tuple[1])    # ditto, can't use the 0th element because it's a key
-        for element in tuple:
-            col_char = [[False]*rows]*cols
-            row_counter = 0
-            if (tuple.index(element) == 0 and key == None):
-                key = chr(int(element))
-            else:
-                this_row = []
-                for col_counter in range(len(element)):
-                        this_bit = True if element[col_counter] == '1' else False
-                        this_row.append(this_bit)
-                        col_char[row_counter][col_counter] = this_bit
-                row.append(this_row)
-            row_counter = row_counter + 1
-        character_dict[key] = [row,list(zip(*row))]
+    if not (args.infile and args.outfile):
+        raise ValueError("missing parameters, use -h for help")
 
-print( character_dict['a'] )
+    savefont(loadfont(args.infile), args.outfile)
+
+
 
